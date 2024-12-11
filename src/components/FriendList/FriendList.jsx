b8c1eaeafa6b36b116abda01853f6e60b80e54fc
Maksym <suzdalcevmaksym@gmail.com>
2024-12-11 18:22:05 +0200

    styles

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import s from './FriendList.module.css'
 
 export const FriendsList = ({friends}) => {
     return <section>
         <div>
-            <ul class="friend-list">
+            <ul className={s.friendList}>
                 {friends.map(friend => (
-                    <li id={friend.id}>
-                    <span>{friend.isOnline }</span>
-                    {/* <span className={isOnline ? s.online : s.offline}></span> */}
-                    <img src={friend.avatar} alt={`User avatar for ${friend.name}`} width="48"></img>
-                    <p>{friend.name}</p>
+                    <li key={friend.id} className={s.item}>
+                    <span className={friend.isOnline ? s.online : s.offline}></span>
+                    <img className={s.avatar} src={friend.avatar} alt={`User avatar for ${friend.name}`} width="48"></img>
+                    <p className={s.name}>{friend.name}</p>
                     </li>
                 ))}
             </ul>
