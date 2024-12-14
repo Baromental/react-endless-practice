434fd8816dca92b8dbe51fe9af72fac0cfd0e318
Maksym <suzdalcevmaksym@gmail.com>
2024-12-15 00:55:45 +0200

    preparation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,5 @@
-import { Profile } from "./Profile/Profile"
-import { Statistics } from "./Statistics/Statistics"
-import { FriendsList } from "./FriendList/FriendList"
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory"
-
-import user from "../assets/user.json"
-import dataStatistics from "../assets/data.json"
-import friends from "../assets/friends.json"
-import transactions from "../assets/transactions.json"
-
-function App(){
-  return <div>
-    <Profile user={user}/>
-    <Statistics dataStatistics={dataStatistics}/>
-    <FriendsList friends={friends}/>
-    <TransactionHistory transactions={transactions}/>
-  </div>
+function App() {
+  return <div></div>;
 }
 
-export default App
+export default App;
