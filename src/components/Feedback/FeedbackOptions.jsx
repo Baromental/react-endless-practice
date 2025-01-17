76dfdcbb72148db99d9a398a13034879256c03bd
Maksym <suzdalcevmaksym@gmail.com>
2025-01-17 15:41:13 +0200

    section

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-export const FeedbackOptions = ({ onChangaMark }) => {
+export const FeedbackOptions = ({ onChangeFeedback }) => {
   return (
     <div>
-      <h2>Please leave feedback</h2>
       <ul>
         <li>
-          <button onClick={() => onChangaMark('good')}>Good</button>
+          <button onClick={() => onChangeFeedback('good')}>Good</button>
         </li>
         <li>
-          <button onClick={() => onChangaMark('neutral')}>Neutral</button>
+          <button onClick={() => onChangeFeedback('neutral')}>Neutral</button>
         </li>
         <li>
-          <button onClick={() => onChangaMark('bad')}>Bad</button>
+          <button onClick={() => onChangeFeedback('bad')}>Bad</button>
         </li>
       </ul>
     </div>
