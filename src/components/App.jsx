ff0ad1351f92fa584b309cd5d52fb7d60bc4038c
Maksym <150538968+Baromental@users.noreply.github.com>
2025-02-15 23:55:02 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,14 @@
+import { Searchbar } from './Gallery/Searchbar';
+import { ImageGallery } from './Gallery/ImageGallery';
+import { Button } from './Gallery/Button';
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Searchbar onSubmit="onSubmit" />
+      <ImageGallery />
+      <Button />
+    </div>
+  );
 }
 
 export default App;
