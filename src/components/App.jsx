e71e6ae270d8e18c561400cb8799e5a55b8f9eeb
Maksym <150538968+Baromental@users.noreply.github.com>
2025-02-21 13:50:12 +0200

    searchbar logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import { Searchbar } from './Gallery/Searchbar';
-import { ImageGallery } from './Gallery/ImageGallery';
-import { Button } from './Gallery/Button';
-import { fetchPosts } from 'services/api';
+import Searchbar from './Gallery/Searchbar';
+// import { ImageGallery } from './Gallery/ImageGallery';
+import { Loader } from './Gallery/Loader';
+// import { Button } from './Gallery/Button';
+import { fetchPhoto } from 'services/api';
 
 export default class App extends Component {
+  state = {
+    query: '',
+    pictures: 0,
+    totalPictures: 0,
+    loading: false,
+    error: null,
+  };
+
   async componentDidMount() {
-    const data = await fetchPosts();
+    try {
+      this.setState({ loading: true });
+      const { pictures, totalPictures } = await fetchPhoto();
+      this.setState({ pictures, totalPictures });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ loading: false });
+    }
   }
+
+  async componentDidUpdate() {}
+
+  handleSetQuery = query => {
+    this.setState({ query });
+  };
+
   render() {
+    const { items, loading } = this.state;
     return (
       <div>
-        {/* <Searchbar onSubmit="onSubmit" />
-        <ImageGallery />
-        <Button /> */}
+        <Searchbar handleSetQuery={this.handleSetQuery} />
+        {/* <ImageGallery /> */}
+        {loading && <Loader />}
+        {/* {items.length ? <Button /> : null} */}
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export const fetchPosts = async () => {
-  const { data } = await axios.get('posts', {
+export const fetchPhoto = async query => {
+  const { data } = await axios.get('', {
     params: {
-      page: 0,
-      per_page: 8,
+      key: '42006022-41a20d969efbb704c546dcbcd',
+      q: query,
+      page: 1,
+      per_page: 12,
     },
   });
   console.log(data);
