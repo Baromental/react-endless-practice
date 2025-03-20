5be86da85226266a612882722580064dc24568c3
Maksym <150538968+Baromental@users.noreply.github.com>
2025-03-20 17:57:38 +0200

    functions of useState and useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState  } from 'react';
+import React, { useEffect, useState } from 'react';
 import Searchbar from './Gallery/Searchbar';
 import { ImageGallery } from './Gallery/ImageGallery';
 import { Loader } from './Gallery/Loader';
@@ -19,68 +19,85 @@ export default function App() {
   //   content: 0,
   // };
 
-const [query, setQuery] = useState('');
-const [pictures, setPictures] = useState([]);
-const [totalPictures, setTotalPictures] = useState(0);
-const [page, setPage] = useState(1);
-const [loading, setLoading] = useState(false);
-const [error, setError] = useState(null);
-const [isOpen, setIsOpen] = useState(false);
-const [content, setContent] = useState(0);
+  const [query, setQuery] = useState('');
+  const [pictures, setPictures] = useState([]);
+  const [totalPictures, setTotalPictures] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [content, setContent] = useState(0);
 
-  async componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
+  // async componentDidUpdate(prevProps, prevState) {
+  //   if (
+  //     prevState.query !== this.state.query ||
+  //     prevState.page !== this.state.page
+  //   ) {
+  //     try {
+  //       this.setState({ loading: true });
+  //       const { hits, totalHits } = await await fetchPictures(
+  //         this.state.query,
+  //         this.state.page
+  //       );
+  //       this.setState(prev => ({
+  //         pictures: [...prev.pictures, ...hits],
+  //         totalPictures: totalHits,
+  //       }));
+  //     } catch (error) {
+  //     } finally {
+  //       this.setState({ loading: false });
+  //     }
+  //   }
+  // }
+
+  useEffect(() => {
+    const getData = async () => {
       try {
-        this.setState({ loading: true });
-        const { hits, totalHits } = await await fetchPictures(
-          this.state.query,
-          this.state.page
-        );
-        this.setState(prev => ({
-          pictures: [...prev.pictures, ...hits],
-          totalPictures: totalHits,
-        }));
+        setLoading(true);
+        const { hits, totalHits } = await fetchPictures(query, page);
+        setPictures(prev => [...prev, ...hits]);
+        setTotalPictures(totalHits);
       } catch (error) {
+        setError(error);
       } finally {
-        this.setState({ loading: false });
+        setLoading(false);
       }
-    }
-  }
+    };
+    getData();
+  }, [query, page]);
 
-  handleSetQuery = query => {
-    this.setState({ query, pictures: [], page: 1 });
+  const handleSetQuery = query => {
+    setQuery(query);
+    setPictures([]);
+    setPage(page);
+    // this.setState({ query, pictures: [], page: 1 });
   };
 
-  handleLoadMore = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
+  const handleLoadMore = () => {
+    setPage(prev => prev + 1);
+    // this.setState(prev => ({ page: prev.page + 1 }));
   };
 
-  handleToggleModal = () => {
-    this.setState(prev => ({ isOpen: !prev.isOpen }));
+  const handleToggleModal = () => {
+    setIsOpen(prev => !prev);
+    // this.setState(prev => ({ isOpen: !prev.isOpen }));
   };
 
-  handleSeeBigPicture = content => {
-    this.setState({ isOpen: true, content });
+  const handleSeeBigPicture = content => {
+    setIsOpen(true);
+    setContent(content);
+    // this.setState({ isOpen: true, content });
   };
 
- 
-    return (
-      <div className={s.App}>
-        <Searchbar handleSetQuery={this.handleSetQuery} />
-        <ImageGallery
-          pictures={pictures}
-          openModal={this.handleSeeBigPicture}
-        />
-        {loading && !pictures.length && <Loader />}
-        {pictures.length && pictures.length < totalPictures ? (
-          <Button handleLoadMore={this.handleLoadMore} />
-        ) : null}
-        {isOpen && (
-          <Modal closeModal={this.handleToggleModal} content={content} />
-        )}
-      </div>
-    );
+  return (
+    <div className={s.App}>
+      <Searchbar handleSetQuery={handleSetQuery} />
+      <ImageGallery pictures={pictures} openModal={handleSeeBigPicture} />
+      {loading && !pictures.length && <Loader />}
+      {pictures.length && pictures.length < totalPictures ? (
+        <Button handleLoadMore={handleLoadMore} />
+      ) : null}
+      {isOpen && <Modal closeModal={handleToggleModal} content={content} />}
+    </div>
+  );
 }
