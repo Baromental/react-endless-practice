d6de3335a2dd1bdb750bd5442be94b3617229522
Maksym <150538968+Baromental@users.noreply.github.com>
2025-03-31 22:20:15 +0300

    base markup

---
+++ src/pages/MovieInfo/MovieInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const MovieInfo = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+        <button></button>
+        <ul>
+          <li></li>
+        </ul>
+      </form>
+    </div>
+  );
+};
