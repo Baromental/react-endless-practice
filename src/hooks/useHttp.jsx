5029900bd3128c9f9fc20b6804df041e9d6bfc91
Maksym <150538968+Baromental@users.noreply.github.com>
2025-04-03 21:52:19 +0300

    Update useHttp.jsx

--- src/hooks/useHttp.jsx
+++ src/hooks/useHttp.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-const useHttp = () => {
-  return <div>useHttp</div>;
+export const useHttp = (fn, param) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fn(param)
+      .then(data => setData(data))
+      .catch(err => setError(err.message));
+  }, [fn, param]);
+  return [data, setData, error];
 };
-
-export default useHttp;
