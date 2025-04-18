a64a117988d12a46f650e1d1121fd6b450c61542
Maksym <150538968+Baromental@users.noreply.github.com>
2025-04-18 22:51:01 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { createStore } from 'redux';
 import { phonebookReducer } from './phonebook/reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(phonebookReducer, composeWithDevTools());
+export const store = createStore(
+  phonebookReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
