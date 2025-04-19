7db4a18e41e0de7880c40731d670281ce5ca49a3
Maksym <150538968+Baromental@users.noreply.github.com>
2025-04-19 15:58:48 +0300

    Update reducers.js

--- src/redux/phonebook/reducers.js
+++ src/redux/phonebook/reducers.js
@@ -1,3 +1,4 @@
+import { createReducer } from '@reduxjs/toolkit';
 import { addContact, removeContact, setFilter } from './actions';
 
 const initialState = {
@@ -5,26 +6,41 @@ const initialState = {
   filter: '',
 };
 
-export const phonebookReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case addContact.type:
-      return {
-        ...state,
-        contacts: [...state.contacts, action.payload],
-      };
-    case removeContact.type:
-      return {
-        ...state,
-        contacts: state.contacts.filter(
-          contact => contact.id !== action.payload
-        ),
-      };
-    case setFilter.type:
-      return {
-        ...state,
-        filter: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+// export const phonebookReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case addContact.type:
+//       return {
+//         ...state,
+//         contacts: [...state.contacts, action.payload],
+//       };
+//     case removeContact.type:
+//       return {
+//         ...state,
+//         contacts: state.contacts.filter(
+//           contact => contact.id !== action.payload
+//         ),
+//       };
+//     case setFilter.type:
+//       return {
+//         ...state,
+//         filter: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+export const phonebookReducer = createReducer(initialState, builder => {
+  builder
+    .addCase(addContact, (state, action) => {
+      state.contacts.push(action.payload);
+    })
+    .addCase(removeContact, (state, action) => {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    })
+    .addCase(setFilter, (state, action) => {
+      state.filter = action.payload;
+    });
+});
