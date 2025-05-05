17735a95b643aff76f6f0f46a8969445ebc7bd56
Maksym <150538968+Baromental@users.noreply.github.com>
2025-05-05 22:16:26 +0300

    Update operation.js

--- src/redux/auth/operation.js
+++ src/redux/auth/operation.js
@@ -1,14 +1,14 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { contactsApi } from 'axiosConfug/contactsApi';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import { contactsApi } from 'axiosConfug/contactsApi';
 
-export const registerThunk = createAsyncThunk(
-  'register',
-  async (credentials, thunkApi) => {
-    try {
-      const { data } = await contactsApi.post('/users/signup', credentials);
-      return data;
-    } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
-    }
-  }
-);
+// export const registerThunk = createAsyncThunk(
+//   'register',
+//   async (credentials, thunkApi) => {
+//     try {
+//       const { data } = await contactsApi.post('/users/signup', credentials);
+//       return data;
+//     } catch (error) {
+//       return thunkApi.rejectWithValue(error.message);
+//     }
+//   }
+// );
