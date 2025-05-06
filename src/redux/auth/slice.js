1d2a8fda8274eecc5754baec3da8e8b0bc036707
Maksym <150538968+Baromental@users.noreply.github.com>
2025-05-06 22:31:19 +0300

    extraReducer

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { registerThunk } from './operation';
 
 const initialState = {
   user: {
@@ -14,6 +15,14 @@ const initialState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState,
+  extraReducers: builder => {
+    builder.addCase(registerThunk.fulfilled, (state, { payload }) => {
+      state.user.name = payload.user.name;
+      state.user.email = payload.user.email;
+      state.token = payload.token;
+      state.isLoggedIn = true;
+    });
+  },
 });
 
 export const authReducer = authSlice.reducer;
